ebdc0d88bc315674c4a2c3547a2a8e63b6ed6a1a
Braulin Pires <braulinpires@hotmail.com>
2023-11-24 17:24:16 +0000

    create a react app clean and installed tailwind css

---
+++ rosa/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="">Here goes the others components</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
