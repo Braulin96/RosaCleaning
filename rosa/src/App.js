0a40a814b15f03a3b98ad9cabfa7fabadf52f68a
Braulin Pires <braulinpires@hotmail.com>
2023-11-28 22:36:13 +0000

    clean the App component

--- rosa/src/App.js
+++ rosa/src/App.js
@@ -1,11 +1,9 @@
-import Navbar from "./components/TestNav";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p className="">Here goes the others components</p>
-        <Navbar />
       </header>
     </div>
   );
