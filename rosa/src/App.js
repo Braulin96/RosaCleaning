7b6ad6879295c128382e6096070422c1c1482ec6
Braulin Pires <braulinpires@hotmail.com>
2023-11-28 09:00:43 +0000

    create a Nav test component

--- rosa/src/App.js
+++ rosa/src/App.js
@@ -1,8 +1,11 @@
+import Navbar from "./components/TestNav";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p className="">Here goes the others components</p>
+        <Navbar />
       </header>
     </div>
   );
