450b2bd3c66a443b9c5a74bfcdd42fef71ef2c28
Braulin Pires <braulinpires@hotmail.com>
2023-11-28 22:43:33 +0000

    created a Navbar in shared components

--- rosa/src/App.js
+++ rosa/src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/shared/Navbar"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <p className="">Here goes the others components</p>
       </header>
