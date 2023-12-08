f3ddfe36107dfbf854ea573a3cbeca8c2d9dceae
Braulin Pires <braulinpires@hotmail.com>
2023-12-08 15:52:36 +0000

    set the my-20 for the div container

--- rosa/src/components/Testimonial.js
+++ rosa/src/components/Testimonial.js
@@ -2,8 +2,8 @@ import React from "react";
 import Profile from "../assets/profile.jpeg";
 import { VscQuote } from "react-icons/vsc";
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const test = [
   {
@@ -24,18 +24,21 @@ const test = [
     quote: "My home Carla has never looked so spotless and fresh! Thank you!",
     position: "Director at Berska",
   },
+  {
+    name: "Carla Paula",
+    profile: Profile,
+    quote: "My home Carla has never looked so spotless and fresh! Thank you!",
+    position: "Director at Berska",
+  },
 ];
 
 const ModuleTestimonial = ({ quote, name, position, profile }) => {
   return (
     <>
-      <div className="w-52 flex flex-col bg-[#d1d2d1] h-60 mx-auto p-4 shadow-md rounded-lg my-auto">
+      <div className="w-52 flex flex-col bg-gray-100 h-60 mx-auto p-4 shadow-md rounded-lg my-auto">
         <div className="my-auto space-y-2">
           <VscQuote color="#e5e7eb" width={40} size={30} />
-          <p className="mx-2">
-            {/* My home has never looked so spotless and fresh! Thank you! */}
-            {quote}
-          </p>
+          <p className="mx-2">{quote}</p>
         </div>
 
         <div className="border-t-2 flex mt-auto">
@@ -61,18 +64,20 @@ const ModuleTestimonial = ({ quote, name, position, profile }) => {
 const Testimonial = () => {
   return (
     <>
-      <div className="flex relative bg-gradient-to-t from-[#bcbdbb] via-white to-white mx-auto flex-wrap h-[600px] ">
-        <div className="flex max-w-7xl justify-around w-full mx-auto">
-          
-          {test.map((details, index) => (
-            <ModuleTestimonial
-              key={index}
-              quote={details.quote}
-              name={details.name}
-              position={details.position}
-              profile={details.profile}
-            />
-          ))}
+      <div className="flex relative bg-gradient-to-t from-gray-200 via-white to-white mx-auto flex-wrap h-[600px] ">
+        <div className="flex flex-col max-w-7xl w-full mx-auto my-20">
+          <h1 className="text-4xl mb-20 text-gray-700 w-full">Testimonial</h1>
+          <div className="flex">
+            {test.map((details, index) => (
+              <ModuleTestimonial
+                key={index}
+                quote={details.quote}
+                name={details.name}
+                position={details.position}
+                profile={details.profile}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
