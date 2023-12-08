7e57ebce6678819da1cd0a017762e01eca4f7673
Braulin Pires <braulinpires@hotmail.com>
2023-12-08 16:51:25 +0000

    set the sizes for the navbar and the logo

--- rosa/src/components/shared/Navbar.js
+++ rosa/src/components/shared/Navbar.js
@@ -7,17 +7,17 @@ const Nav = () => {
     <Navbar fluid rounded className="max-w-7xl mx-auto">
       <Navbar.Brand href="https://flowbite-react.com">
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          <img width={240} src={Logo} alt="logo" />
+          <img width={300} src={Logo} alt="logo" />
         </span>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="#" active>
+        <Navbar.Link className="text-lg" href="#" active>
           Home
         </Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Price</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
+        <Navbar.Link className="text-lg" href="#">Services</Navbar.Link>
+        <Navbar.Link className="text-lg" href="#">Price</Navbar.Link>
+        <Navbar.Link className="text-lg" href="#">Contact</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
     </>
