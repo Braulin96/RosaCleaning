fa7307adae607d27c7bef2c1c141833795299829
Braulin Pires <braulinpires@hotmail.com>
2023-12-22 14:49:27 +0000

    removed the skeleton

--- rosa/src/components/shared/HeroSection.js
+++ rosa/src/components/shared/HeroSection.js
@@ -1,17 +1,10 @@
 import Hero from "../../assets/heroSection.png";
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
 
 const HeroSection = () => {
   return (
     <div className="bg-gradient-to-b from-white via-white to-[#d5effc]">
       <div className="flex justify-around h-[700px] max-w-7xl mx-auto px-4 flex-wrap">
         <div className="my-auto space-y-4">
-         {
-          <p> Testing</p> ||   <Skeleton />
-
-         } 
-        
           <h1 className=" sm:text-3xl md:text-4xl text-2xl sm:leading-relaxed">
             Welcome to RosaCleaning <br />
             Where Cleanliness Meets Excellence!
@@ -23,21 +16,17 @@ const HeroSection = () => {
           </div>
         </div>
         <div className="sm:mt-auto">
-          {
-            <img
+          <img
             className="sm:w-[30rem] sm:min-w-[20rem] sm:max-w-[30rem] max-w-[20rem] hover:opacity-90"
             src={Hero}
             alt="painting"
-          /> || <Skeleton />
-
-          }
-          
+          />
         </div>
         <div className="sm:hidden w-full -mt-20">
-            <button className="px-6 py-2 w-full bg-[#DF66A7] rounded-full font-bold text-white hover:bg-opacity-90">
-              Request Service
-            </button>
-          </div>
+          <button className="px-6 py-2 w-full bg-[#DF66A7] rounded-full font-bold text-white hover:bg-opacity-90">
+            Request Service
+          </button>
+        </div>
       </div>
     </div>
   );
