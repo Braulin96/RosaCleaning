4440e5058a8808195cd7519a13a01b20729edb14
Braulin Pires <braulinpires@hotmail.com>
2024-01-03 12:53:38 +0000

    created module component in ChooseUs section

--- rosa/src/components/shared/ChooseUs.js
+++ rosa/src/components/shared/ChooseUs.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { BsShieldLockFill } from "react-icons/bs";
 
-const Module = () => {
+const Module = ({ icon, title, children }) => {
   return (
     <>
       <div className="flex space-x-6 max-w-md">
         <div className="bg-white h-24 my-auto flex shrink-0 aspect-square rounded-full">
-        <BsShieldLockFill className="m-auto" size={55} color="#0171E3" />
+          {icon &&
+            React.createElement(icon, {
+              className: "m-auto",
+              size: 55,
+              color: "#0171E3",
+            })}
         </div>
         <div className="my-auto">
-          <p className="font-medium text-2xl"> Title </p>
+          <p className="font-medium text-2xl"> {title} </p>
           <p className="mt-2">
-            The concept of marketing support product range, regardless of the
-            cost market trends
+            {/* The concept of marketing support product range, regardless of the
+            cost market trends */}
+            {children}
           </p>
         </div>
       </div>
@@ -26,8 +32,18 @@ const ChooseUs = () => {
       <div className="bg-[#E6F1FC]">
         <div className="flex justify-around h-[700px] max-w-7xl mx-auto px-4 flex-wrap py-20">
           <div>
-            <h1 className="text-[40px] font-medium">Why Choose Us</h1>
-            <Module />
+            <h1 className="text-[40px] font-medium mx-auto flex justify-center">Why Choose Us</h1>
+           
+            <div className="flex">
+              <Module title="Testing" icon={BsShieldLockFill}>
+                The concept of marketing support product range, regardless of
+                the cost market trends
+              </Module>
+              <Module title="Testing" icon={BsShieldLockFill}>
+                The concept of marketing support product range, regardless of
+                the cost market trends
+              </Module>
+            </div>
           </div>
         </div>
       </div>
