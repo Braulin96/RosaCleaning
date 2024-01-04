d846fd281038bf72803da8aabf541f530591dd4d
Braulin Pires <braulinpires@hotmail.com>
2024-01-04 10:11:26 +0000

    created a Steps component

---
+++ rosa/src/components/shared/Steps.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Steps = () => {
+  return (
+    <div className="bg-[#F7F7F7] sm:mx-4 mx-2 rounded-2xl">
+
+   
+    <div className="flex h-[700px] max-w-7xl mx-auto px-4 flex-wrap py-20">
+      <h1 className="text-[40px] font-medium flex sm:mb-16 mb-12 mr-auto text-[#00284F]">
+        Unlocking the magic behind our <br />
+        commitment is as easy as 1-2-3-4.
+      </h1>
+    </div>
+    </div>
+  );
+};
+export default Steps;
