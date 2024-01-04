98c1d9768e0e5fb41f96f6b7f25466cb0808f4f9
Braulin Pires <braulinpires@hotmail.com>
2024-01-04 11:48:03 +0000

    hide the iphone image for smaller screens

--- rosa/src/components/shared/Steps.js
+++ rosa/src/components/shared/Steps.js
@@ -5,6 +5,7 @@ import { RiShoppingCartFill } from "react-icons/ri";
 import { IoCallSharp } from "react-icons/io5";
 import { GiPayMoney } from "react-icons/gi";
 import { MdCleaningServices } from "react-icons/md";
+import Iphone from "components/assets/images/iPhone.png";
 
 const Module = ({ title, description, icon }) => {
   return (
@@ -34,62 +35,56 @@ const Steps = () => {
   return (
     <div className="bg-[#F7F7F7] sm:mx-4 mx-2 rounded-2xl">
       <div className="flex h-[700px] max-w-7xl mx-auto px-4 flex-wrap py-20">
-        <div>
-          <h1 className="text-[40px] font-medium flex sm:mb-16 mb-12 mr-auto text-[#00284F]">
-            Unlocking the magic behind our <br />
-            commitment is as easy as 1-2-3-4.
-          </h1>
+        <div className="flex">
           <div>
-            <div className="flex flex-col">
-              <Module
-                title="Order Service"
-                icon={RiShoppingCartFill}
-                description="Order our service by calling (here) or by email (here)"
-              />
-              {/* <div className="my-2">
-            <HiOutlineDotsVertical
-              color="#0171E3"
-              size={24}
-              className="ml-5 opacity-50"
-            />
-             <HiOutlineDotsVertical
-              color="#0171E3"
-              size={24}
-              className="ml-5 -mt-0.5 opacity-50"
-            />
+            <div className="flex justify-between">
+              <div>
+                <h1 className="text-[40px] font-medium flex sm:mb-16 mb-12 mr-auto text-[#00284F]">
+                  Unlocking the magic behind our <br />
+                  commitment is as easy as 1-2-3-4.
+                </h1>
+                <div className="flex flex-col">
+                  <Module
+                    title="Order Service"
+                    icon={RiShoppingCartFill}
+                    description="Order our service by calling (here) or by email (here)"
+                  />
+                  <HiOutlineDotsVertical
+                    color="#0171E3"
+                    size={24}
+                    className="ml-5 my-2 opacity-60"
+                  />
 
-            </div> */}
-              <HiOutlineDotsVertical
-                color="#0171E3"
-                size={24}
-                className="ml-5 my-2 opacity-60"
-              />
-
-              <Module
-                title="Confirmation Call"
-                icon={IoCallSharp}
-                description="Call you to confirm the service request and the data)"
-              />
-              <HiOutlineDotsVertical
-                color="#0171E3"
-                size={24}
-                className="ml-5 my-2 opacity-60"
-              />
-              <Module
-                title="Payment"
-                icon={GiPayMoney}
-                description="After confirmation the client will make the payment"
-              />
-              <HiOutlineDotsVertical
-                color="#0171E3"
-                size={24}
-                className="ml-5 my-2 opacity-60"
-              />
-              <Module
-                title="Service day"
-                icon={MdCleaningServices}
-                description="Proceed to service request"
-              />
+                  <Module
+                    title="Confirmation Call"
+                    icon={IoCallSharp}
+                    description="Call you to confirm the service request and the data)"
+                  />
+                  <HiOutlineDotsVertical
+                    color="#0171E3"
+                    size={24}
+                    className="ml-5 my-2 opacity-60"
+                  />
+                  <Module
+                    title="Payment"
+                    icon={GiPayMoney}
+                    description="After confirmation the client will make the payment"
+                  />
+                  <HiOutlineDotsVertical
+                    color="#0171E3"
+                    size={24}
+                    className="ml-5 my-2 opacity-60"
+                  />
+                  <Module
+                    title="Service day"
+                    icon={MdCleaningServices}
+                    description="Proceed to service request"
+                  />
+                </div>
+              </div>
+              <div className="my-auto h-full text-center justify-center md:flex hidden">
+                <img className="my-auto h-full flex" width={500} src={Iphone} />
+              </div>
             </div>
           </div>
         </div>
