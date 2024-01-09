ab7868fbf71e6c08a307ca9c34c53fb09887f25d
Braulin Pires <braulinpires@hotmail.com>
2024-01-09 11:40:09 +0000

    added all elements to footer

--- rosa/src/components/shared/Footer.js
+++ rosa/src/components/shared/Footer.js
@@ -1,35 +1,60 @@
 import React from "react";
-import { MdOutlineMail } from "react-icons/md";
-import { FaPhoneAlt } from "react-icons/fa";
-import Contact from '../../assets/contact.png'
+import WomanCalling from "../assets/images/womanCalling.png";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
-    <div className="flex bg-gray-900 relative mx-auto flex-wrap h-[600px]">
-      <div className="flex  max-w-7xl w-full mx-auto my-20 gap-8">
-        <div className="w-1/2 rounded-md h-full my-auto">
-          <h1 className="text-4xl mt-4 mb-8 text-gray-700 w-full">
-            Get in touch!
+    <div className="pt-24 pb-12 bg-[#F7F7F7]">
+      <div className="flex w-full ">
+        <div className="flex-grow">
+          <h1 className="text-[40px] font-bold flex justify-center text-[#00284F] mb-6">
+            Get In Touch!
           </h1>
-          <div className="flex flex-col space-y-4">
-          <div className="">
-            <div className="flex gap-x-2">
-              <MdOutlineMail size={25} className="my-auto" />
-              <p className="text-lg"> Email</p>
+          <div className="flex mb-4">
+            <div className="mx-auto flex space-x-4">
+              <FaSquareFacebook size={40} color="#78ccf5" />
+              <RiInstagramFill size={40} color="#78ccf5" />
+              <IoLogoLinkedin size={40} color="#78ccf5" />
             </div>
-            <p className="ml-6">RosaCleaning@hotmail.com</p>
           </div>
-          <div>
-            <div className="flex gap-x-2">
-              <FaPhoneAlt size={20} className="my-auto" />
-              <p className="text-lg "> Contact Number</p>
+          <div className="flex">
+            <div className="mx-auto flex space-x-32 mt-8">
+              <div>
+                <p className="text-lg font-semibold mb-2">Quick Links :</p>
+                <ul className="flex flex-col gap-y-2">
+                  <li>Home</li>
+                  <li>How it works</li>
+                  <li>Pricing</li>
+                  <li>Faq's</li>
+                </ul>
+              </div>
+              <div className="">
+                <div>
+                  <p className="text-lg font-semibold mb-2">Business Hours :</p>
+                  <p>7:30 am - 8:30 pm, Mon-Sat</p>
+                  <p>8 am - 5 pm, Sun</p>
+                </div>
+                <div>
+                  <p className="text-lg font-semibold mb-2">Location</p>
+                  <p>70 M31 Piccadilly Manchester</p>
+                </div>
+              </div>
             </div>
-            <p className="ml-6">07809222182</p>
-          </div>
           </div>
         </div>
-        <div className="w-1/2 rounded-md h-full mx-auto flex -mt-36">
-            <img width={400} src={Contact} alt="woman calling mx-auto flex"/> 
+        <div className=" w-1/3">
+          <img width={350} src={WomanCalling} />
+        </div>
+      </div>
+      <div className="flex border-t border-opacity-70 border-[#D1D2D1] max-w-7xl  mt-12 mx-auto ">
+        <div className="mx-auto flex justify-between w-full mt-4">
+          <p className="text-sm">
+            @2023 - Rosa Cleaning Services - All righths reserved
+          </p>
+          <button className="text-sm">Terms and Privacy</button>
         </div>
       </div>
     </div>
