0e13818f71d79afb0a2ece842927d85786fc68c2
Braulin Pires <braulinpires@hotmail.com>
2024-01-09 15:41:38 +0000

    removed text-xl to buttons in hero section

--- rosa/src/components/shared/HeroSection.js
+++ rosa/src/components/shared/HeroSection.js
@@ -15,16 +15,16 @@ const HeroSection = () => {
       <div className="flex justify-around sm:py-32 py-20 max-w-7xl mx-auto px-4 flex-wrap space-y-12">
         <div className="my-auto space-y-4">
           <h1 className="text-[40px] font-semibold">
-            The best{" "}
-            <span className="text-[#0171E3] mr-2">Cleaning Service</span>
+            The best
+            <span className="text-[#0171E3] ml-2">Cleaning Service</span>
             <br />
             Transforming Spaces, Elevating Lives
           </h1>
           <div className="flex space-x-10">
-            <button className="px-6 py-2 bg-[#0171E3] rounded-md font-bold text-white hover:bg-opacity-90 sm:w-auto w-full mt-4 text-xl">
+            <button className="px-6 py-2 bg-[#0171E3] rounded-md font-bold text-white hover:bg-opacity-90 sm:w-auto w-full mt-4">
               Let's Talk
             </button>
-            <button className="px-6 py-2 bg-[#E6F1FC] rounded-md font-bold text-[#0171E3] hover:text-opacity-80  sm:w-auto w-full mt-4 text-xl">
+            <button className="px-6 py-2 bg-[#E6F1FC] rounded-md font-bold text-[#0171E3] hover:text-opacity-80  sm:w-auto w-full mt-4">
               Services
             </button>
           </div>
@@ -32,13 +32,13 @@ const HeroSection = () => {
             <div className="flex gap-x-2">
               <ImCheckboxChecked className="my-auto" color="#E6F1FC" />
               <p className="text-sm">
-                We’ re Local - Family Owned and Operated
+                We’re Local - Family Owned and Operated
               </p>
             </div>
             <div className="flex gap-x-2 mt-2">
               <ImCheckboxChecked className="my-auto" color="#E6F1FC" />
               <p className="text-sm">
-                We’ re Quick - Same Day Pick Up and Drop Off
+                We’re Quick - Same Day Pick Up and Drop Off
               </p>
             </div>
           </div>
