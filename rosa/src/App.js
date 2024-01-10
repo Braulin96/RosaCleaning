cd4d857586857014763dba1b2dfcd5e1b681e9ab
Braulin Pires <braulinpires@hotmail.com>
2024-01-10 09:00:23 +0000

    clean App.js

--- rosa/src/App.js
+++ rosa/src/App.js
@@ -1,23 +1,26 @@
 import Navbar from "./components/shared/Navbar";
 import HeroSection from "./components/shared/HeroSection";
-import Services from "./components/shared/Services";
 import Testimonial from "./components/Testimonial";
-import Price from "./components/shared/Price";
-import Footer from "./components/shared/Footer";
 import ChooseUs from "components/shared/ChooseUs";
 import OurService from "components/shared/OurService";
+import Steps from "components/shared/Steps";
+import Pricing from "components/shared/Pricing";
+import Features from "components/shared/Features";
+import Dedicating from "components/shared/Dedicating";
+import Footer from "components/shared/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <ChooseUs/> 
+      <ChooseUs />
       <OurService />
-      {/* {/* <Services />
-      <Testimonial /> */}
-      {/* <Price/> */}
-      {/* <Footer/>   */}
+      <Steps />
+      <Pricing />
+      <Features />
+      <Dedicating />
+      <Footer />
     </div>
   );
 }
