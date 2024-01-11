d2aa2dd1e098b4d60b635fd700881981821e5a00
Braulin Pires <braulinpires@hotmail.com>
2024-01-11 17:11:08 +0000

    create a hovered state to anime the profile pictures

--- rosa/src/components/shared/Testimonials.js
+++ rosa/src/components/shared/Testimonials.js
@@ -1,39 +1,87 @@
-import React from "react";
+import { useState } from "react";
 import { RiDoubleQuotesL } from "react-icons/ri";
 import Andrea from "../../assets/andreaProfile.jpeg";
 
+// const Module = () => {
+//   return (
+//     <div className="w-full flex justify-center relative">
+//       <div className="border-2 flex flex-col border-[#e0e1e0] rounded-lg w-80 h-72 overflow-hidden shadow-md">
+//         <div className="flex space-x-4 px-6 py-8">
+//           <div className="w-1/3">
+//             <RiDoubleQuotesL color="#e0e1e0" size={60} />
+//           </div>
+//           <div className="flex-grow">
+//             <p className="text-[#414241]">
+//               Is simply dummy text of the printing and typesetting industry.
+//               Lorem Ipsum has been the industry's standard dummy text ever.
+//             </p>
+//           </div>
+//         </div>
+//         <div className="mt-auto bg-gray-100 p-4 flex justify-between border-t-2 border-[#e0e1e0] py-3">
+//           <div className="flex flex-col gap-y-1">
+//             <p className="text-sm font-semibold"> Mr Jason Mraz</p>
+//             <p className="text-xs"> CEO at Curbside Prophet Inc.</p>
+//           </div>
+//           <div className="w-20 h-20 overflow-hidden rounded-full -mt-10 border-2 border-white">
+//             <img
+//               className="w-full h-full object-cover"
+//               src={Andrea}
+//               alt="andrea profile picture"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 const Module = () => {
-  return (
-    <div className="w-full flex justify-center">
-      <div className="border-2 flex flex-col border-[#e0e1e0] rounded-lg w-80 h-72 overflow-hidden shadow-md">
-        <div className="flex space-x-4 px-6 py-8">
-          <div className="w-1/3">
-            <RiDoubleQuotesL color="#e0e1e0" size={60} />
+    const [isHovered, setIsHovered] = useState(false);
+  
+    const imageStyle = {
+      transition: "transform 1s ease", // Add a transition for a smooth effect
+      transform: isHovered ? "translateY(-15px)" : "translateY(0)",
+    };
+  
+    return (
+      <div className="w-full flex justify-center relative">
+        <div
+          onMouseOver={() => setIsHovered(true)}
+          onMouseOut={() => setIsHovered(false)}
+          className="border-2 flex flex-col border-[#e0e1e0] rounded-lg w-80 h-72 overflow-hidden shadow-md"
+        >
+          <div className="flex space-x-4 px-6 py-8">
+            <div className="w-1/3">
+              <RiDoubleQuotesL color="#e0e1e0" size={60} />
+            </div>
+            <div className="flex-grow">
+              <p className="text-[#414241]">
+                Is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever.
+              </p>
+            </div>
           </div>
-          <div className="flex-grow">
-            <p className="text-[#414241]">
-              Is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever.
-            </p>
-          </div>
-        </div>
-        <div className="mt-auto bg-gray-100 p-4 flex justify-between border-t-2 border-[#e0e1e0] py-3">
-          <div className="flex flex-col gap-y-1">
-            <p className="text-sm font-semibold"> Mr Jason Mraz</p>
-            <p className="text-xs"> CEO at Curbside Prophet Inc.</p>
-          </div>
-          <div className="w-20 h-20 overflow-hidden rounded-full -mt-10 border-2 border-white">
-            <img
-              className="w-full h-full object-cover"
-              src={Andrea}
-              alt="andrea profile picture"
-            />
+          <div className="mt-auto bg-gray-100 p-4 flex justify-between border-t-2 border-[#e0e1e0] py-3">
+            <div className="flex flex-col gap-y-1">
+              <p className="text-sm font-semibold"> Mr Jason Mraz</p>
+              <p className="text-xs"> CEO at Curbside Prophet Inc.</p>
+            </div>
+            <div
+              className="w-20 h-20 overflow-hidden rounded-full -mt-10 border-2 border-white"
+              style={imageStyle}
+            >
+              <img
+                className="w-full h-full object-cover"
+                src={Andrea}
+                alt="andrea profile picture"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+
 
 function Testimonials() {
   return (
@@ -44,23 +92,24 @@ function Testimonials() {
             What Our Clients Says
           </h1>
         </div>
-        <div className="mx-auto">
-          <div className="flex gap-x-24">
-            <div className="ml-20">
+        <div className="mx-auto relative">
+          <div className="flex gap-x-10 z-10">
+            <div className="ml-10">
               <Module />
             </div>
-            <div className="mt-20">
+            <div className="mt-10">
               <Module />
             </div>
           </div>
-          <div className="flex gap-x-24">
+          <div className="flex gap-x-10">
             <div>
               <Module />
             </div>
-            <div className="mt-20">
+            <div className="mt-10">
               <Module />
             </div>
           </div>
+          {/* <div className="w-52 h-52 -z-10 rounded-full bg-[#d1d2d1] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-3xl"></div> */}
         </div>
       </div>
     </div>
