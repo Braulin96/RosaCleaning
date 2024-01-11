6d8087ccfa145c38d31eb27dfa960faa5345319a
Braulin Pires <braulinpires@hotmail.com>
2024-01-11 13:13:52 +0000

    created a testimonial section

---
+++ rosa/src/components/shared/Testimonials.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Testimonials() {
+  return (
+    <div className="bg-cyan-100 ">
+      <div className="flex h-fit py-20 sm:py-32 max-w-7xl mx-auto px-4 flex-wrap">
+        <div className="w-full">
+          <h1 className="text-[40px] font-medium flex sm:mb-24 mb-12 mr-auto text-[#00284F] sm:text-left text-center">
+            What Our Clients Says 
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Testimonials;
