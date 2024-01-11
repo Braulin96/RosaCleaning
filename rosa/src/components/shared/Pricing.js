e1d5c625a72f6f5407d65b5eeda1acb5fc4ffec5
Braulin Pires <braulinpires@hotmail.com>
2024-01-11 22:36:48 +0000

    removed empty spaces

--- rosa/src/components/shared/Pricing.js
+++ rosa/src/components/shared/Pricing.js
@@ -81,7 +81,7 @@ const Module = ({ title, className, serviceType, price }) => {
 const Pricing = () => {
   return (
     <>
-      <div className="flex  h-fit py-20 sm:py-32 max-w-7xl mx-auto px-4 flex-wrap">
+      <div className="flex h-fit py-20 sm:py-32 max-w-7xl mx-auto px-4 flex-wrap">
         <div className="w-full">
           <h1 className="text-[40px] font-medium flex sm:mb-24 mb-12 mr-auto text-[#00284F] sm:text-left text-center">
             We offer unparalleled <br />
