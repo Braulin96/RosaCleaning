d9bfe565db0efebe4cb95e7694a0dc65d16b47a0
Braulin Pires <braulinpires@hotmail.com>
2024-01-18 16:41:54 +0000

    pass elements as duration, delay as a props

--- rosa/src/components/shared/animation/ScrollAnimation.js
+++ rosa/src/components/shared/animation/ScrollAnimation.js
@@ -1,25 +1,26 @@
 // Note: hooks:
+import { useEffect } from "react";
 // Note: components:
 import AOS from "aos";
 import "aos/dist/aos.css";
-import React, { useEffect } from "react";
 
-const ScrollAnimation = ( { data, easing, duration, delay, children }) => {
+const ScrollAnimation = ({ data, easing, duration, delay, children }) => {
   useEffect(() => {
     AOS.init({
       disable: "phone",
       //duration: 900,
-      //once: true,
+      once: true,
       // easing: "ease-out-cubic",
     });
   }, []);
   return (
     <div
       data-aos={data}
+      data-aos-easing={easing}
       //data-aos-offset="200"
       //data-aos-easing={easing}
-      data-aos-duration="1000"
-      data-aos-delay="500"
+      data-aos-duration={duration}
+      data-aos-delay={delay}
     >
       {children}
     </div>
