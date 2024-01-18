0d45417b36aa727d5effd46a12db4849b5455c0a
Braulin Pires <braulinpires@hotmail.com>
2024-01-18 21:00:05 +0000

    set the text colors to text-[#00284F] in Features component

--- rosa/src/components/Features.js
+++ rosa/src/components/Features.js
@@ -24,17 +24,17 @@ const features = [
 
 const Module = ({ question, answer }) => {
   return (
-    <div className="flex flex-grow">
+    <div className="flex flex-grow text-[#00284F]">
       <Accordion className="w-full">
         <AccordionSummary
-          expandIcon={<IoIosArrowDown color="#8d8f8c" />}
+          expandIcon={<IoIosArrowDown color="#00284F" />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>{question}</Typography>
+          <Typography className="text-[#00284F]">{question}</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>{answer}</Typography>
+          <Typography className="text-[#00284F]">{answer}</Typography>
         </AccordionDetails>
       </Accordion>
     </div>
@@ -43,11 +43,11 @@ const Module = ({ question, answer }) => {
 
 const Features = () => {
   return (
-    <div className="bg-[#F7F7F7] sm:mx-4 mx-2 rounded-2xl" id="faqs">
+    <div className="bg-[#F7F7F7] sm:mx-4 mx-2 rounded-2xl text-[#00284F]" id="faqs">
       <div className="flex h-fit py-24 max-w-7xl mx-auto px-4 flex-wrap w-full">
         <div className="flex py-20 max-w-7xl mx-auto sm:px-36 px-0 flex-wrap w-full">
           <div className="w-full">
-            <h1 className="text-[40px] font-medium mx-auto text-center flex justify-center sm:mb-16 mb-12 text-[#00284F]">
+            <h1 className="text-[40px] font-medium mx-auto text-center flex justify-center sm:mb-16 mb-12">
               We Cover Most <br /> Asked Questions here
             </h1>
             <div className="mx-auto flex flex-col space-y-12">
